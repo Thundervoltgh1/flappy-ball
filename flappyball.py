7a4bed892ab34f585516f82b472237311e818a59
Thundervoltgh1 <eniolusi@gmail.com>
2024-08-03 15:04:47 +0100

    Update flappyball.py

--- flappyball.py
+++ flappyball.py
@@ -1,8 +1,8 @@
 import pgzrun
 import random
 TITLE= "Flappy ball"
-WIDTH= 800
-HEIGHT=600
+WIDTH= 600
+HEIGHT=500
 r=random.randint(0,255)
 g=random.randint(0,255)
 b=random.randint(0,255)
@@ -19,7 +19,7 @@ class Ball:
         pos=(self.x,self.y)
         screen.draw.filled_circle(pos,self.radius,CLR)
 ball=Ball(50,100)
-GRAVITY=2000.0
+GRAVITY=1000.0
 def draw():
     screen.clear()
     ball.draw()
@@ -28,4 +28,17 @@ def update(dt):
     uy=ball.vy
     ball.vy+=GRAVITY*dt
     ball.y+=(uy+ball.vy)*0.5*dt 
+
+    if ball.y>HEIGHT-ball.radius:
+        ball.y=HEIGHT-ball.radius
+        ball.vy=-ball.vy*0.5
+        #inelastic collision
+    ball.x+=ball.vx*dt
+    if ball.x<ball.radius or ball.x>WIDTH-ball.radius:
+        ball.vx=-ball.vx
+    if ball.y<ball.radius:
+        ball.vy=-ball.vy
+def on_key_down(key):
+    if key==keys.B and ball.y>ball.radius:
+        ball.vy=-500
 pgzrun.go()
